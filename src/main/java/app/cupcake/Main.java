46d8c82ee538ba65dbc2321f658d9116567583e8
Magnewei <magnewei@icloud.com>
2024-04-02 11:01:51 +0200

    Quick change to Class UML

--- src/main/java/app/cupcake/Main.java
+++ src/main/java/app/cupcake/Main.java
@@ -6,10 +6,10 @@ import app.cupcake.Persistence.ConnectionPool;
 import app.cupcake.Thymeleaf.ThymeleafConfig;
 
 public class Main {
-    private static final String USER = "USERNAME";
-    private static final String PASSWORD = "PASSWORD";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "databasename";
+    private static final String DB = "cupcake";
     public static void main(String[] args) {
         // Initializing Javalin and Jetty webserver
         Javalin app = Javalin.create(config -> {
