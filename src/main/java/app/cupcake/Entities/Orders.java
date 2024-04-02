3b1186e9ec6ca15aa4a44484ecc6e9448240b182
christiansmac <christianglindemannmeggele@gmail.com>
2024-04-02 10:03:23 +0200

    hej

---
+++ src/main/java/app/cupcake/Entities/Orders.java
@@ -0,0 +1,4 @@
+package app.cupcake.Entities;
+
+public class Orders {
+}
---
+++ src/main/java/app/cupcake/Persistence/OrdersMapper.java
@@ -0,0 +1,10 @@
+package app.cupcake.Persistence;
+
+import app.cupcake.Entities.Orders;
+
+import java.util.List;
+
+public class OrdersMapper {
+    public static List<Orders> getAllTasksPerUser(int userId, ConnectionPool connectionPool) {
+    }
+}
